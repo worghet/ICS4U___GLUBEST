2d7eae5c6a23c6f0bad9bc322a9cd522231daf82
worghet <worker.forget@gmail.com>
2025-03-29 15:21:54 -0400

    got webcam running

--- backend/Agent.java
+++ backend/Agent.java
@@ -1,8 +1,13 @@
 package backend;
 
-
+import com.github.sarxos.webcam.*;
 import com.fazecast.jSerialComm.SerialPort;
+
+import java.io.File;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
+import javax.imageio.ImageIO;
 
 public class Agent {    
 
@@ -10,28 +15,54 @@ public class Agent {
     private static final String PORT_NAME = "/dev/ttyACM0"; // linux-specific
     final static Scanner input = new Scanner(System.in);
     
-    final SerialPort serialPort = SerialPort.getCommPort(PORT_NAME); // indicate com port
+    SerialPort serialPort; // indicate com port
+    Webcam webcam;
 
     public static void main(String[] args) {
+
+
+        // Webcam webcam = Webcam.getDefault();
+		// if (webcam != null) {
+		// 	System.out.println("Webcam: " + webcam.getName());
+		// } else {
+		// 	System.out.println("No webcam detected");
+		// }
+
         // start agent
         System.out.println("creating agent..");
         Agent agent = new Agent();
-        if (agent.initializedSerialPort()) {
 
-            String requestArduino;
-            while (true) {
-                requestArduino = input.nextLine();
-                if (requestArduino.equals(FEED_KEYWORD)) {
-                    try {
-                        agent.performArduinoRotation();
-                    }
-                    catch (Exception e) {
-                        System.out.println("sum went wrong");
-                    }
-                }
-    
-            }
+        try {
+            System.out.println("setting up cam");
+            agent.webcam = Webcam.getDefault();
+            System.out.println("opened cam");
+            agent.webcam.open();
+            
+            ImageIO.write(agent.webcam.getImage(), "PNG", new File("hello-world.png"));
+            System.out.println("did it!");
         }
+        catch (Exception e) {
+            System.out.println("cam went wrong");
+        }
+
+
+        // agent.serialPort = SerialPort.getCommPort(PORT_NAME);
+        // if (agent.initializedSerialPort()) {
+
+        //     String requestArduino;
+        //     while (true) {
+        //         requestArduino = input.nextLine();
+        //         if (requestArduino.equals(FEED_KEYWORD)) {
+        //             try {
+        //                 agent.performArduinoRotation();
+        //             }
+        //             catch (Exception e) {
+        //                 System.out.println("sum went wrong");
+        //             }
+        //         }
+    
+        //     }
+        // }
 
     }
 
---
+++ tester.java
@@ -0,0 +1,35 @@
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+import java.awt.Dimension;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+import com.github.sarxos.webcam.Webcam;
+
+public class tester {
+    public static void main(String[] args) {
+        Webcam webcam = Webcam.getDefault();
+		webcam.setViewSize(WebcamResolution.VGA.getSize());
+
+		WebcamPanel panel = new WebcamPanel(webcam);
+		panel.setFPSDisplayed(true);
+		panel.setDisplayDebugInfo(true);
+		panel.setImageSizeDisplayed(true);
+		panel.setMirrored(true);
+
+		JFrame window = new JFrame("Test webcam panel");
+		window.add(panel);
+		window.setResizable(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();
+		window.setVisible(true);
+    }
+}
